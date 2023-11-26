b77c329aa9c514c57477dfa3fc600d1af86a88c5
dyno-pvt <japanacc1976@gmail.com>
2023-11-26 15:47:30 +0530

    UPDATE

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-print("koyeb")
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+
+def start(update, context):
+    update.message.reply_text("Press the button to send a message to the bot owner:", reply_markup=reply_markup)
+
+def main():
+    updater = Updater("6759114451:AAGWHSxbJGZwP_a4OFrREL-eDVUGyXVYm0U", use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+main()
