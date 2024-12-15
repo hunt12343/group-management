a985913c3ad5720ad338718c11b8035d74bcd83c
hunt12343 <adifreefire00007@gmail.com>
2024-12-15 19:51:51 +0530

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,6 +1,7 @@
 from telegram import Update, ChatPermissions
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import logging
+import os
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
